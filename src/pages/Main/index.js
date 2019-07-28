308ddf0bfee09c0ec7acbaa1adcc5d4908d9d5cd
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2019-07-28 18:30:07 -0300

    Implementa a funcionalidade adicionar repositórios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+export default class Main extends Component {
+  state = {
+    newRepository: '',
+    repositories: [],
+    loading: false,
+  };
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
-        <SubmitButton disabled>
-          <FaPlus color="FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleInputChange = e => {
+    this.setState({
+      newRepository: e.target.value,
+    });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({
+      loading: true,
+    });
+
+    const { newRepository, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepository}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepository: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepository, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepository}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
